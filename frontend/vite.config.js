9eba91c3b4881bf28ae6fea28e9e525f478c1fde
erodriguezm1 <edgarjaviii@gmail.com>
2025-07-23 12:16:26 -0500

    docker funcionando sin base de datos aun

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Importante para Docker: el servidor escucha en todas las interfaces
+    port: 3000, // Fuerza el puerto 3000
+  }
 })
