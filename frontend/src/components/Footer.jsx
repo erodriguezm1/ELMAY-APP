8107b5bb78218f36fe4505cf22b1ccb405df82a1
erodriguezm1 <edgarjaviii@gmail.com>
2025-08-05 14:34:20 -0500

    login completo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,16 @@ import './Footer.css';
 
 function Footer() {
   return (
-        <h1>nobovi</h1>
-
+    <div>
+        <footer>
+          <p>
+            Created with <i className="fa fa-heart"></i> byr
+            <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
+            - Read how I created this and how you can join the challenge
+            <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
+          </p>
+        </footer>
+    </div>
   );
 }
 
