196c1775563fe70c9a5e0169e0e15d6e1e452aad
erodriguezm1 <edgarjaviii@gmail.com>
2025-08-05 16:34:39 -0500

    cambios en vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Importante para Docker: el servidor escucha en todas las interfaces
-    port: 3000, // Fuerza el puerto 3000
+    host: 'elmay.redinterna.local', //true, // Importante para Docker: el servidor escucha en todas las interfaces
+    protocol: 'ws', // Usa 'ws' para WebSocket sobre HTTP
+    //port: 3000, // Fuerza el puerto 3000
     allowedHosts: ['elmay.redinterna.local'],
   }
 })
