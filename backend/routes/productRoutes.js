8b71610dac7d8009673abf1aa0778a8eda56783f
erodriguezm1 <edgarjaviii@gmail.com>
2025-08-28 13:06:21 -0500

    cambios de componentes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,17 @@
+// ELMAY-APP/backend/routes/productRoutes.js
+const express = require('express');
+const router = express.Router();
+const { createProduct, getProducts, getSellerProducts, } = require('../controllers/productController');
+const { protect } = require('../middleware/authMiddleware'); 
+// Ruta para crear un producto. Requiere que el usuario esté autenticado.
+router.route('/').post(protect, createProduct);
+// @route   GET /api/products
+// @desc    Ruta pública para obtener todos los productos
+router.get('/', getProducts);
+// @route   GET /api/products/seller
+// @desc    Ruta privada para obtener los productos del vendedor logueado
+router.get('/seller', protect, getSellerProducts);
+
+
+
+module.exports = router;
